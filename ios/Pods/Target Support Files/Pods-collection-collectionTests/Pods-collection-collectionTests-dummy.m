3daf9ad2a8263a4992d697f1a995a8af35fe0766
Clément <clement.bock@epitech.eu>
2020-04-23 19:19:25 +0200

    yolo restart from here

---
+++ ios/Pods/Target Support Files/Pods-collection-collectionTests/Pods-collection-collectionTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_collection_collectionTests : NSObject
+@end
+@implementation PodsDummy_Pods_collection_collectionTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-collection-tvOS/Pods-collection-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_collection_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_collection_tvOS
+@end
