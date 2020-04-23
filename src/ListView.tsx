d2faff3bdd3317290b58b8a07d60ba2436f27cc8
Clément <clement.bock@epitech.eu>
2020-04-23 18:07:32 +0200

    no message

---
+++ src/ListView.tsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import { Text, View } from 'react-native';
+
+const ListView = () => {
+  return (
+    <View
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}
+    >
+      <Text>LIST VIEW HEHE</Text>
+    </View>
+  );
+};
+
+export default ListView
