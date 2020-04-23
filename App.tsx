2b651e9e0ed376908f3bffb93b3e0232f6675076
Clément <clement.bock@epitech.eu>
2020-04-23 17:47:07 +0200

    updated dependencies

--- App.tsx
+++ App.tsx
@@ -26,8 +26,6 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import truc from 'root' // delete this later
-
 const App = () => {
   const usingHermes = typeof HermesInternal === 'object' && HermesInternal !== null;
   return (
@@ -44,7 +42,6 @@ const App = () => {
             </View>
           )}
           <View style={styles.body}>
-            <Text>{undefined?.truc?.a?.b?.c?.d ? 'A' : 'B'}</Text>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
